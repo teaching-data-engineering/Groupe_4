d19d5dff34886e5197dd57fb03c203f5593ec334
« Slazz26 » <paul.vallee1@icloud.com>
2024-10-07 16:40:59 +0200

    Scrapping for multiple days

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # Prédiction de la participation aux évènements à Berlin
 branche dev_paul
 
-https://www.bandsintown.com/this-month/fetch-next/upcomingEvents?came_from=257&utm_medium=web&utm_source=home&utm_campaign=top_event&sort_by_filter=Number+of+RSVPs&concerts=true&page=2&longitude=13.41053&latitude=52.52437&genre_query=all-genres
--- events.py
+++ events.py
@@ -1,9 +1,10 @@
 import requests
 import time
+import re 
 
-# date : today, all-dates, this-month ,this-week, 
-def get_events(page=2, start_date="2024-10-07" ,end_date = "2024-10-30" ):
-    
+
+# date : today, all-dates, this-month, this-week
+def get_json(start_date, end_date, page=1):
     all_events = []
 
     # Initialisation des paramètres de la requête
@@ -18,42 +19,57 @@ def get_events(page=2, start_date="2024-10-07" ,end_date = "2024-10-30" ):
         "longitude": "13.41053",
         "latitude": "52.52437",
         "genre_query": "all-genres",
-        "date" : start_date + "T00%3A00%3A00%2C" + end_date + "T23%3A00%3A00"
+        "date": f"{start_date}T00%3A00%3A00%2C{end_date}T23%3A00%3A00"
     }
 
     # On spécifie l'URL de la requête puis on l'envoie
-    url = "https://www.bandsintown.com/choose-dates/fetch-next/upcomingEvents"
+    url = "https://www.bandsintown.com/choose-date/fetch-next/upcomingEvents"
     response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"}, params=params)
 
     # Vérification de la réponse
     if response.status_code != 200:
-        print(f"Erreur lors de la requête pour la page {page}")
+        print(f"Erreur lors de la requête pour la page {page}: {response.status_code}")
+        print(f"Response Text: {response.text}")
         return all_events
 
     data = response.json()
 
-    # On récupère les événements
-    events = data.get("events")
+    return data 
 
-    if not events:
-        print(f"Aucun événement trouvé pour la page {page}")
-        return all_events
 
-    # On enregistre les événements
-    all_events.extend(events)
 
-    # On affiche les événements
-    for event in events:
-        artist = event.get("artistName")
-        venue = event.get("venueName")
-        date = event.get("startsAt")
-        print(artist, venue, date)
+def check_next_page(data):
+    next_url = data.get("urlForNextPageOfEvents")
+    if next_url is not None:
+        return re.search(r"page=\d+",next_url ).group(0).split("=")[1]
+    else :
+        return None
 
+
+def get_all_events(start_date, end_date):
+   
+    all_events = []
+
+    #Creer un vecteur contenant toute les dates entre start_date et end_date
+    all_dates = [time.strftime("%Y-%m-%d", time.strptime(str(i), "%j")) for i in range(time.strptime(start_date, "%Y-%m-%d").tm_yday, time.strptime(end_date, "%Y-%m-%d").tm_yday + 1)]
+    page = 1 
+    for date in range(len(all_dates)-1):
+        data = get_json(page=page, start_date=all_dates[date], end_date=all_dates[date+1])
+        while data:
+            all_events.extend(data.get("events"))
+            next_page = check_next_page(data)
+            if next_page is not None:
+                page = get_json(page=next_page,start_date=all_dates[date], end_date=all_dates[date+1])
+            else:
+                break
     return all_events
 
 
+
+
+
+
+
 if '__main__' == __name__:
-    data = get_events(page=1)
-    print(data)
-import requests
-import time
+    data = get_all_events(start_date = "2024-10-08", end_date = "2024-10-10")
+   
