05182bf045eac171be302408938527514b3aaf91
« Slazz26 » <paul.vallee1@icloud.com>
2024-10-08 13:23:01 +0200

    Modif Load_data

--- load_data.py
+++ load_data.py
@@ -11,13 +11,13 @@ def load_data_events(pkl_file):
     return df
 
 
-def main(repertory): 
+def load_all_events(): 
+    repertory = 'data'
     pkl_files = [os.path.join(repertory, f) for f in os.listdir(repertory) if f.endswith('.pkl')]
     dfs = list(map(load_data_events, pkl_files))
     df = reduce(lambda df1, df2: pd.concat([df1, df2]), dfs)
     return df 
 
-
 if '__main__' == __name__:
-    df = main('data')
-    print(df)
+   df = load_all_events()
+   df.head()
