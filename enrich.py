6bcaba65aa2097fc58e6af4ed0af0e5a576f92ca
« Slazz26 » <paul.vallee1@icloud.com>
2024-10-08 10:30:19 +0200

    Modif Load_data.py

---
+++ enrich.py
@@ -0,0 +1,7 @@
+
+
+def info_artist(artist):
+    """
+    Get information about an artist using the Spotify API
+    """
+    return artist
--- load_data.py
+++ load_data.py
@@ -1,14 +1,23 @@
+import os
 import pandas as pd
 import pickle
-
+from functools import reduce
 
 def load_data_events(pkl_file): 
-    with open('concerts.pkl', 'rb') as f:
+    with open(pkl_file, 'rb') as f:
         concerts = pickle.load(f)
     df = pd.DataFrame(concerts)
     df = df.loc[:, ~df.apply(lambda col: col.astype(str).str.startswith('https://')).any()]
     return df
 
+
+def main(repertory): 
+    pkl_files = [os.path.join(repertory, f) for f in os.listdir(repertory) if f.endswith('.pkl')]
+    dfs = list(map(load_data_events, pkl_files))
+    df = reduce(lambda df1, df2: pd.concat([df1, df2]), dfs)
+    return df 
+
+
 if '__main__' == __name__:
-    df = load_data_events('concerts.pkl')
-    print(df.head())
+    df = load_data_events('data')
+    print(df.shape())
