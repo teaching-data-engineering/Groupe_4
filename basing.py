280260e7f917057b4cbe603efc5a991c81cdd218
« Slazz26 » <paul.vallee1@icloud.com>
2024-10-08 16:29:06 +0200

    All pipeline clean

---
+++ basing.py
@@ -0,0 +1,6 @@
+import pandas_gbq
+from google.oauth2 import service_account 
+
+def data_to_gbq(df):
+    credentials = service_account.Credentials.from_service_account_file( 'credientials/sa-key-group-4.json', )
+    pandas_gbq.to_gbq(df,"dataset_groupe_4.non_enrich", project_id="ai-technologies-ur2",credentials = credentials ) 
