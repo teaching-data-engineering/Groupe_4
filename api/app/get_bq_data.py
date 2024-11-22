e112ae79f2a78e77c00873c113f1a0ea16e03b98
« Slazz26 » <paul.vallee1@icloud.com>
2024-11-22 14:04:57 +0100

    modif chemin credentials

--- api/app/get_bq_data.py
+++ api/app/get_bq_data.py
@@ -4,7 +4,7 @@ from google.oauth2 import service_account
 
 def get_bq_data():
     # Chemin vers votre fichier de clé JSON
-    key_path = r'/Users/paulvallee/Desktop/MAS/M2/Tech IA/Projet/Groupe_4/credentials/sa-key-group-4.json'
+    key_path = r'credentials/sa-key-group-4.json'
     
     # Créez les credentials
     credentials = service_account.Credentials.from_service_account_file(key_path)
